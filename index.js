be1e73b86a41c394fdbd046dbd68ce8af88571cb
enricadirado <100517394+enricadirado@users.noreply.github.com>
2023-05-02 01:12:16 +0200

    1 file modified

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ la classe archivio contiene
 i libri sono creati con la classe libro
 */
 
+var stringa_ricerca='wal';
+var new_archivio=[];
 
 class OggettoLibro {
   constructor(titolo, autore, posizione, nominativo) {
@@ -28,10 +30,28 @@ class OggettoArchivio {
     this.oggettoLibro2 = new OggettoLibro("antonia pozzi", "desiderio di cose leggere", "S1L5P2", "davide di rado");
     this.oggettoLibro3 = new OggettoLibro("pierluigi cappello", "stato di quiete", "S3L2P5", "");
   }
-  search () {
+  nameContainss(str) {
+
+    this.forEach(myFun, str);
+    function myFun(libro){
+      if (this.libro.titolo.match(str)){
+        new_archivio.push(libro.titolo);
+        new_archivio.push(libro.autore);
+      } 
+    }
   }
+nameContains(str){
+  ;
+}
+
 }
 
 var oggettoArchivio = new OggettoArchivio();
 console.log(oggettoArchivio.oggettoLibro1.autore); 
 console.log(oggettoArchivio.oggettoLibro2.titolo);
+console.log(oggettoArchivio);
+console.log(oggettoArchivio.nameContains(stringa_ricerca));
+
+/*
+return this.oggettoLibro1.titolo.includes(str);
+*/
