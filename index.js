f8a9774ca1c47465c186431910ac74bf30d11320
enricadirado <100517394+enricadirado@users.noreply.github.com>
2023-05-01 18:28:48 +0200

    2 files modified

--- index.js
+++ index.js
@@ -2,5 +2,15 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+class libro {
+  constructor(autore, titolo, posizione, nominativo) {
+    this.autore = autore;
+    this.titolo = titolo;
+    this.posizione = posizione;
+    this.nominativo= nominativo;
+  }
+}
+
+const libro1 = new libro("walt whitman", "leaves of grass", "S3L3P4", "enrica di rado");
+const libro2 = new libro("antonia pozzi", "desiderio di cose leggere", "S1L5P2", "davide di rado");
+const libro3 = new libro("pierluigi cappello", "stato di quiete", "S3L2P5", "");
